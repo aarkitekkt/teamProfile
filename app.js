20ced00b9be1492bf1ee68b3310ad0675eb36631
aarkitekkt <johnson.brycej@gmail.com>
2020-02-03 12:19:19 -0700

    determine employee type and save data to array

--- app.js
+++ app.js
@@ -36,14 +36,57 @@ function promptUser() {
             }
         ])
         .then(val => {
-            var e = new Employee(val.name, val.id, val.email);
-            employees.push(e);
-            console.log(employees);
+            let e = new Employee(val.name, val.id, val.email);
+            if (val.role == "manager") {
+                inquirer
+                    .prompt(
+                        {
+                            type: "input",
+                            name: "officeNumber",
+                            message: "What is your office number?"
+                        }
+                    )
+                    .then(val => {
+                        const mgr = new Manager(e.name, e.id, e.email, val.officeNumber);
+                        mgr.getRole();
+                        employees.push(mgr);
+                        console.log(employees);
+                    })
+            } else if (val.role == "engineer") {
+                inquirer
+                    .prompt(
+                        {
+                            type: "input",
+                            name: "github",
+                            message: "What is your GitHub User Name?"
+                        }
+                    )
+                    .then(val => {
+                        const eng = new Engineer(e.name, e.id, e.email, val.github);
+                        eng.getRole();
+                        employees.push(eng);
+                        console.log(employees);
+                    })
+            } else if (val.role == "intern") {
+                inquirer
+                    .prompt(
+                        {
+                            type: "input",
+                            name: "school",
+                            message: "What school did you go to?"
+                        }
+                    )
+                    .then(val => {
+                        const int = new Intern(e.name, e.id, e.email, val.school);
+                        int.getRole();
+                        employees.push(int);
+                        console.log(employees);
+                    })
+            }
         })
+
+
 }
-
-
-
 
 
 
