30009745e57b30adfb3a2692e62360af65b988de
aarkitekkt <johnson.brycej@gmail.com>
2020-02-05 22:23:02 -0700

    take employee and write to html file

---
+++ Lib/prompts.js
@@ -0,0 +1,56 @@
+const employeePrompts = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    }, {
+        type: "input",
+        name: "id",
+        message: "What is your ID number?"
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    }, {
+        type: "list",
+        name: "role",
+        message: "What is your role with the company?",
+        choices: [
+            "engineer",
+            "manager",
+            "intern"
+        ]
+    }];
+
+const managerPrompt =
+{
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office number?"
+};
+
+const internPrompt =
+{
+    type: "input",
+    name: "school",
+    message: "What school did you attend?"
+};
+
+const engineerPrompt =
+{
+    type: "input",
+    name: "github",
+    message: "What is your GitHub user name?"
+};
+
+const anotherEmployee =
+{
+    type: "confirm",
+    name: "anotherEmployee",
+    message: "Would you like to add another employee?"
+};
+
+const prompts = { employeePrompts, managerPrompt, internPrompt, engineerPrompt, anotherEmployee }
+
+
+module.exports = prompts;
--- app.js
+++ app.js
@@ -3,78 +3,63 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-// const Prompt = require("./lib/Prompt")
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const prompts = require("./lib/prompts")
 
 const employees = [];
 
+
+
 getEmployee();
 
+
+
+function buildTeam() {
+
+    const html = generateHTML(employees);
+
+    writeFileAsync("./output/team.html", html);
+
+    console.log("team page built!")
+
+}
+
 //Function to build array of employees from user input.
-function getEmployee() {
-    promptUser()
+async function getEmployee() {
+
+    await promptUser()
         .then(val => {
             let e = new Employee(val.name, val.id, val.email);
             if (val.role === "manager") {
-                return inquirer
-                    .prompt(
-                        {
-                            type: "input",
-                            name: "officeNumber",
-                            message: "What is your office number?"
-                        }
-                    )
+                return inquirer.prompt(prompts.managerPrompt)
                     .then(val => {
                         const mgr = new Manager(e.name, e.id, e.email, val.officeNumber);
                         mgr.getRole();
                         employees.push(mgr);
-                        console.log("Employee has been added!");
-                        console.log(employees);
                     })
 
             } else if (val.role === "engineer") {
                 return inquirer
-                    .prompt(
-                        {
-                            type: "input",
-                            name: "github",
-                            message: "What is your GitHub User Name?"
-                        }
-                    )
+                    .prompt(prompts.engineerPrompt)
                     .then(val => {
                         const eng = new Engineer(e.name, e.id, e.email, val.github);
                         eng.getRole();
                         employees.push(eng);
-                        console.log("Employee has been added!");
-                        console.log(employees);
                     })
 
             } else if (val.role === "intern") {
-                return inquirer
-                    .prompt(
-                        {
-                            type: "input",
-                            name: "school",
-                            message: "What school did you go to?"
-                        }
-                    )
+                return inquirer.prompt(prompts.internPrompt)
                     .then(val => {
                         const int = new Intern(e.name, e.id, e.email, val.school);
                         int.getRole();
                         employees.push(int);
-                        console.log("Employee has been added!");
-                        console.log(employees);
                     })
             }
         })
-        .then(function () {
-            return inquirer
-                .prompt(
-                    {
-                        type: "confirm",
-                        name: "anotherEmployee",
-                        message: "Would you like to add another employee?"
-                    }
-                )
+        .then(() => {
+            return inquirer.prompt(prompts.anotherEmployee)
         })
         .then(val => {
             if (val.anotherEmployee == true) {
@@ -82,36 +67,102 @@ function getEmployee() {
                 getEmployee();
             } else {
                 console.log("Employee list has been created!");
-                return
+                console.log(employees);
+                buildTeam();
             }
         })
 }
 
 //function to ask user questions to gather employee information.
 function promptUser() {
-    return inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "name",
-                message: "What is your name?"
-            }, {
-                type: "input",
-                name: "id",
-                message: "What is your ID number?"
-            }, {
-                type: "input",
-                name: "email",
-                message: "What is your email?"
-            }, {
-                type: "list",
-                name: "role",
-                message: "What is your role with the company?",
-                choices: [
-                    "engineer",
-                    "manager",
-                    "intern"
-                ]
-            }
-        ])
+    return inquirer.prompt(prompts.employeePrompts)
 }
+
+function generateHTML(team) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    <h1 id="headerText">TEAM</h1>
+    <div id="employeeCards" class="row">
+    ${team.map(generateCard).join('')}
+    </div>
+    </body>
+</html>`
+}
+
+function generateCard(emp) {
+    if (emp.title === "Engineer") {
+        return `
+    <div class="col s12 m6 l4">
+            <div class="card hoverable" style="border-radius: 25px;">
+                <div class="blue-grey darken-2 center-align white-text"
+                    style="padding: 75px; border-radius: 25px; border: 15px solid white;">
+                    <img src="../assets/devIcon.png" alt="">
+                    <h4 id="name" style="margin-bottom: 15px;">${emp.name}</h4>
+                    <h5>${emp.title}</h5>
+
+                </div>
+                <div class="card-content">
+                    <ul>
+                        <li id="id">id: ${emp.id}</li>
+                        <li id="email">email: ${emp.email}</li>
+                        <li id="github">github: ${emp.github}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>`
+    } else if (emp.title === "Intern") {
+        return `
+    <div class="col s12 m6 l4">
+            <div class="card hoverable" style="border-radius: 25px;">
+                <div class="blue-grey darken-4 center-align white-text"
+                    style="padding: 75px; border-radius: 25px; border: 15px solid white;">
+                    <img src="../assets/devIcon.png" alt="">
+                    <h4 id="name" style="margin-bottom: 15px;">${emp.name}</h4>
+                    <h5>${emp.title}</h5>
+
+                </div>
+                <div class="card-content">
+                    <ul>
+                        <li id="id">id: ${emp.id}</li>
+                        <li id="email">email: ${emp.email}</li>
+                        <li id="school">school: ${emp.school}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>`
+    } else if (emp.title === "Manager") {
+        return `
+    <div class="col s12 m6 l4">
+            <div class="card hoverable" style="border-radius: 25px;">
+                <div class="blue-grey lighten-1 center-align white-text"
+                    style="padding: 75px; border-radius: 25px; border: 15px solid white;">
+                    <img src="../assets/devIcon.png" alt="">
+                    <h4 id="name" style="margin-bottom: 15px;">${emp.name}</h4>
+                    <h5>${emp.title}</h5>
+
+                </div>
+                <div class="card-content">
+                    <ul>
+                        <li id="id">id: ${emp.id}</li>
+                        <li id="email">email: ${emp.email}</li>
+                        <li id="office">office number: ${emp.officeNumber}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>`
+    } else {
+        return "Damn"
+    }
+
+}
+
+
